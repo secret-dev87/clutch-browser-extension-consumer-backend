fe26d19323dd8642797e52a72d1dea8cdfc760e0
secret-dev87 <dev.bruce.87@gmail.com>
2023-08-09 10:22:54 -0400

    updated database

--- migrations/V2__create_table_accounts.sql
+++ migrations/V2__create_table_accounts.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     email          TEXT    NOT NULL,
     wallet_address TEXT    NOT NULL,
     eoa_address    TEXT    NOT NULL,
+    eoa_private_address    TEXT    NOT NULL,
     updated_at     INTEGER NOT NULL
 );
